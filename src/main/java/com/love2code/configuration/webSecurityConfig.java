dcfe68867f8a6709c352b1d01f91379a3f5cc770
hansmbua <hansmbua@yahoo.com>
2023-10-03 08:35:49 +0200

    added configuration

---
+++ src/main/java/com/love2code/configuration/webSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.love2code.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class webSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests((requests) ->
+                        requests.requestMatchers(
+                                new AntPathRequestMatcher("/"),
+                                new AntPathRequestMatcher("/search")
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
+
+
+
+
+
+return http.build();
+    }
+
+
+
+}
